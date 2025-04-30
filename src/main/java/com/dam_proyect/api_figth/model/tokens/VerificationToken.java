14c6ae7b25fc5bd8c4f5161c8d8e97c623ab1757
anxosolidcode <anxo@solid-code.nl>
2025-04-30 12:04:40 +0200

    Add user association and expiry date to VerificationToken entity

--- src/main/java/com/dam_proyect/api_figth/model/tokens/VerificationToken.java
+++ src/main/java/com/dam_proyect/api_figth/model/tokens/VerificationToken.java
@@ -3,6 +3,7 @@ package com.dam_proyect.api_figth.model.tokens;
 import com.dam_proyect.api_figth.model.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 
 import java.time.LocalDateTime;
@@ -13,6 +14,40 @@ public class VerificationToken {
     private String token;
 
     @OneToOne
+    @JoinColumn(nullable = false, name = "user_id")
     private User user;
     private LocalDateTime expiryDate;
+
+    public VerificationToken() {
+    }
+
+    public VerificationToken(String token, User user, LocalDateTime expiryDate) {
+        this.token = token;
+        this.user = user;
+        this.expiryDate = expiryDate;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDateTime expiryDate) {
+        this.expiryDate = expiryDate;
+    }
 }
