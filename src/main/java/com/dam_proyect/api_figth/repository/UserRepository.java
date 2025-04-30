7d96047843397e7f6705814a0ea693f3a3e3088b
anxosolidcode <anxo@solid-code.nl>
2025-04-30 10:34:38 +0200

    Add UserRepository interface for user data access

---
+++ src/main/java/com/dam_proyect/api_figth/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.dam_proyect.api_figth.repository;
+
+import com.dam_proyect.api_figth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
