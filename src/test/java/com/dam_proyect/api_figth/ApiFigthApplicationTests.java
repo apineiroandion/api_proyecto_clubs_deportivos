691d1c4c30d7ac07d35600d1b9c6dbe4eed5d8cf
anxosolidcode <anxo@solid-code.nl>
2025-04-30 08:42:24 +0200

    first commit

---
+++ src/test/java/com/dam_proyect/api_figth/ApiFigthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam_proyect.api_figth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFigthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
