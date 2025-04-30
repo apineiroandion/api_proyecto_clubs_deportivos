b832b3ba850e7801e39e2559f188bf2c0a79c04f
anxosolidcode <anxo@solid-code.nl>
2025-04-30 12:06:12 +0200

    Add VerificationTokenRepository for token management

---
+++ src/main/java/com/dam_proyect/api_figth/repository/VerificationTokenRepository.java
@@ -0,0 +1,9 @@
+package com.dam_proyect.api_figth.repository;
+
+import com.dam_proyect.api_figth.model.tokens.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, String> {
+}
