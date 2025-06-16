cccb835eaeb533284996524940c2d99293cdf22d
anxo <apineiroandion@danielcastelao.org>
2025-06-16 22:10:59 +0200

    Update database connection URL and add TrainingSession entity for managing training sessions

---
+++ src/main/java/com/dam_proyect/api_figth/model/TrainingSession.java
@@ -0,0 +1,31 @@
+package com.dam_proyect.api_figth.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "training_sessions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TrainingSession {
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private LocalDate date;
+
+    @OneToOne
+    private ClubMembership trainer;
+
+    @ManyToOne
+    private Club club;
+
+    @ManyToMany
+    private List<ClubMembership> attendees;
+}
